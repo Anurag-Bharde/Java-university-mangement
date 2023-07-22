6685b5b38a0e7cec463c67807995741b6e420e88
Anurag-Bharde <anwolfragspitter99@gmail.com>
2023-07-22 13:56:33 +0530

    addfiles

---
+++ UNIVERSITY/Splash.java
@@ -0,0 +1,41 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame implements Runnable{
+   Thread t;
+    Splash(){
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("img/Collegeimg.jpg"));
+        Image i2=i1.getImage().getScaledInstance(1000,700,Image.SCALE_DEFAULT);
+        ImageIcon i3= new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        add(image);
+        setVisible(true);
+    t=new Thread(this);
+    t.start();
+        int x=1;
+        for(int i=2;i<=600;i+=4, x+=1) {
+            setLocation(600-((i+x)/2),350-(i/2));
+            setSize(i+3*x, i+x/2);
+
+            try{
+                Thread.sleep(10);
+            } catch (Exception e){
+
+            }
+        }
+    }
+
+    public void run(){
+          try {
+              Thread.sleep(7000);
+              setVisible(false);
+          } catch (Exception e){
+
+          }
+    }
+    public static void main(String[] args) {
+        Splash h=new Splash();
+    }
+
+}
